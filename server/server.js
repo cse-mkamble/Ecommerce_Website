d3ade51877c99cca748f6a5c255ebba71fab7345
cse-mkamble <cse.mkamble@gmail.com>
2021-08-01 14:22:28 +0530

    First Commit

---
+++ server/server.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const connectDB = require('./config/db')
+
+// Import Router
+const authRouter = require("./routes/auth");
+const categoryRouter = require("./routes/categories");
+const productRouter = require("./routes/products");
+const brainTreeRouter = require("./routes/braintree");
+const orderRouter = require("./routes/orders");
+const usersRouter = require("./routes/users");
+const customizeRouter = require("./routes/customize");
+
+// Import Auth middleware for check user login or not~
+const { loginCheck } = require("./middleware/auth");
+
+// Database Connection
+connectDB()
+
+// Middleware
+app.use(morgan("dev"));
+app.use(cookieParser());
+app.use(cors());
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Routes
+app.use("/api", authRouter);
+app.use("/api/user", usersRouter);
+app.use("/api/category", categoryRouter);
+app.use("/api/product", productRouter);
+app.use("/api", brainTreeRouter);
+app.use("/api/order", orderRouter);
+app.use("/api/customize", customizeRouter);
+
+// Run Server
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+  console.log("Server is running on ", PORT);
+});
